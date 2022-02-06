55bcbd00ba84aa5f305f5b5fde16590fcc8d2f9f
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2022-02-06 22:41:37 +0700

    updated web

---
+++ src/pages/discord.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Discord extends React.Component {
+ componentDidMount(){
+     window.location.href = "https://discord.gg/nCQbSag";
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default Discord;
