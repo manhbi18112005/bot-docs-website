6c4c9461bf4435d356640a53a21dbc593bfccb13
MyTツ <myt@nnsvn.me>
2023-08-07 15:05:11 +0700

    updated

--- src/pages/invite.js
+++ src/pages/invite.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Invite extends React.Component {
  componentDidMount(){
-     window.location.href = "https://discord.com/api/oauth2/authorize?client_id=736915194772586598&permissions=8&scope=bot%20applications.commands";
+     window.location.href = "https://discord.com/oauth2/authorize?client_id=736915194772586598&scope=bot+applications.commands+applications.commands.permissions.update&permissions=2080374975";
   }
 
   render() {
